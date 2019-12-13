c4e571cb46a9b30412773c696995eb306879b8be
Fabian Höring <f.horing@criteo.com>
2019-12-13 16:41:54 +0100

    Expose public functions

--- cluster_pack/__init__.py
+++ cluster_pack/__init__.py
@@ -2,3 +2,17 @@
 from ._version import get_versions
 __version__ = get_versions()['version']
 del get_versions
+
+
+from cluster_pack.packaging import (
+    zip_path,
+    upload_env,
+    upload_zip,
+    get_editable_requirements,
+    get_non_editable_requirements,
+    get_default_fs,
+    detect_packer_from_file,
+    Packer,
+    CONDA_PACKER,
+    PEX_PACKER
+)
