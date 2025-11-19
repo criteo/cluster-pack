b7af9dc49450c0437483e1fe823729522c1afb5c
Julien Cuquemelle <j.cuquemelle@criteo.com>
2025-11-19 12:18:41 +0100

    switch to uv for spark testing in docker
    And few fixes in spark tests, simplification of python version handling,
    deferring everything at docker image build
    
    Reordered docker file statements, so heavier layers (java, hadoop) happen
    sooner for a faster local iterations

--- examples/spark-with-S3/scripts/run_spark_example.sh
+++ examples/spark-with-S3/scripts/run_spark_example.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 rm -rf /tmp/pyspark_env
-$1 -m venv /tmp/pyspark_env
-. /tmp/pyspark_env/bin/activate
-pip install -U pip setuptools wheel
-pip install pypandoc<1.8
-pip install s3fs pandas pyspark==$2
-pip install -e /cluster-pack
+uv venv /tmp/pyspark_env
+source /tmp/pyspark_env/bin/activate
+
+uv pip install -U pip setuptools wheel
+uv pip install "pypandoc<1.8" s3fs pandas pyspark==$1
+uv pip install -e /cluster-pack
 
 curr_dir=$(dirname "$0")
 python -V
